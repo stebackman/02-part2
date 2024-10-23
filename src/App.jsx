c40f2aed63159dac5fcb097fbfb3ad30360b13a0
stebackman <stebackman@gmail.com>
2024-10-23 14:21:07 +0300

    gittaus ennen render-kokeilua

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,111 @@
-import Note from "./components/Note";
-const App = ({ notes }) => {
+import { useState, useEffect } from "react";
+import noteService from "./services/notes";
+
+const Footer = () => {
+  const footerStyle = {
+    color: "green",
+    fontStyle: "italic",
+    fontSize: 16,
+  };
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>
+        Note app, Department of Computer Science, University of Helsinki 2023
+      </em>
+    </div>
+  );
+};
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+    });
+  }, []);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+    };
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+      setNewNote("");
+    });
+  };
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+      })
+      .catch((error) => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+        setNotes(notes.filter((n) => n.id !== id));
+      });
+  };
+  const Notification = ({ message }) => {
+    if (message === null) {
+      return null;
+    }
+
+    return <div className="error">{message}</div>;
+  };
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+  const Note = ({ note, toggleImportance }) => {
+    const label = note.important ? "make not important" : "make important";
+
+    return (
+      <li className="note">
+        {note.content}
+        <button onClick={toggleImportance}>{label}</button>
+      </li>
+    );
+  };
+
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => (
-          <Note key={note.id} note={note} />
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+      <Footer />
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,26 +2,37 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-const notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
+import "./index.css";
+/*
+import axios from "axios"; /*
 
+/*const promise = axios.get("http://localhost:3001/notes");
+console.log(promise);
+*/
+/*
+const promise2 = axios.get("http://localhost:3001/foobar");
+console.log(promise2);
+*/
+/*
+const promise = axios.get("http://localhost:3001/notes");
+
+promise.then((response) => {
+  console.log(response);
+});
+*/ /*
 ReactDOM.createRoot(document.getElementById("root")).render(
   <App notes={notes} />
 );
-const result = notes.map((note) => note.content);
-console.log(result);
+axios.get("http://localhost:3001/notes").then((response) => {
+  const notes = response.data;
+  console.log(notes);
+});
+*/
+/*
+axios.get("http://localhost:3001/notes").then((response) => {
+  const notes = response.data;
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <App notes={notes} />
+  );
+});*/
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
